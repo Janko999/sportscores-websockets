c81f4d281405795ad475d6ff901a192bc986cb33
Jane Kostov <jane.kostov@expertallies.com>
2026-03-02 14:29:00 +0200

    app deployed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from 'express';
 import {matchRouter} from "./routes/matches.js";
 import http from "http";
@@ -19,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to SportsCore API');
 });
 
-app.use(securityMiddleware());
+// app.use(securityMiddleware());
 
 app.use('/matches', matchRouter);
 app.use('/matches/:id/commentary', commentaryRouter)
